8ea56600518afcb7bd443fa7aac27efcd10f3d71
zeviberlin <zeviberlin@gmail.com>
2026-07-21 09:55:26 -0400

    just gonna go ahead with the URL support

--- include/llm/retrieveApiCreds.hpp
+++ include/llm/retrieveApiCreds.hpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <string>
 
-// #define API_URL_NAME "FIX_LLM_API_URL"
+#define API_URL_NAME "FIX_LLM_API_URL"
 #define API_KEY_NAME "FIX_LLM_API_KEY"
 
 
@@ -12,10 +12,9 @@ inline std::string retrieveEnv(const char* key) {
   return (var == NULL) ? "" : std::string(var);
 }
 
-// TODO: Implement a system to be able to communicate with any provider, not just opencode
-// [[nodiscard]] static std::string retrieveApiURL() {
-//   return retrieveEnv(API_URL_NAME);
-// }
+[[nodiscard]] static std::string retrieveApiURL() {
+  return retrieveEnv(API_URL_NAME);
+}
 
 [[nodiscard]] static std::string retrieveApiKey() {
   return retrieveEnv(API_KEY_NAME);
